c4feb3cfce0ce8c3ef8bd16f60468e5e7eb466c4
Jan Bühlmann <janbue@janbue.ch>
2023-03-31 06:44:45 +0200

    autostart nextcloud

--- install_i3_config.sh
+++ install_i3_config.sh
@@ -9,10 +9,12 @@ echo "installing i3lock"
 sudo pacman -S i3lock
 
 echo -n "installing i3 config: "
+mkdir -p ~/.config/i3
 ln -sf ~/dotfiles/i3/i3config ~/.config/i3/config
 echo "done"
 
 echo -n "installing i3blocks config: "
+mkdir -p ~/.config/i3blocks
 ln -sf ~/dotfiles/i3/i3blocksconfig ~/.config/i3blocks/config
 echo "done"
 
